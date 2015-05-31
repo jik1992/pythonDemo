772f990dc561c09b704de287e01a59bc4394540d
左韵 <chinajik@gmail.com>
2015-05-31 11:37:13 +0800

    update distutils

--- caption_16_distutils.py
+++ caption_16_distutils.py
@@ -1,1 +1,15 @@
-__author__ = 'zuoyun1'
+#!/usr/bin/env python  
+from distutils.core import setup
+
+setup(  # Distribution meta-data
+        name="testpackage",
+        version="1.0",
+        description="Distutils sample distribution testpackage",
+        # py_modules = [''],
+        packages=['TestPackage'])
+
+
+# pachage cli:
+# python setup.py sdist
+# python setup.py bdist_wininst
+# python setup.py bdist_rpm
